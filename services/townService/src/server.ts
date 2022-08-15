a0eb45d4804c50a7ffead5060982af9025143ac9
Yuecheng Shi <yuechengshi@Yuechengs-MacBook-Pro.local>
2022-08-14 22:16:36 -0700

    add unit tests

--- services/townService/src/server.ts
+++ services/townService/src/server.ts
@@ -11,7 +11,7 @@ const server = http.createServer(app);
 
 addTownRoutes(server, app);
 
-server.listen(process.env.PORT || 8081, () => {
+server.listen(process.env.PORT || 8080, () => {
   const address = server.address() as AddressInfo;
   // eslint-disable-next-line no-console
   console.log(`Listening on ${address.port}`);
